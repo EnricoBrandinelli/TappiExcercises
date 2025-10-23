9536abb6fdb101ac114e79861ba587dd0c959739
EnricoBrandinelli <enrico.brandinelli.stud@ispascalcomandini.it>
2025-10-23 11:45:49 +0200

    commit

---
+++ src/TappiExcercises.Domain/Grade.cs
@@ -0,0 +1,25 @@
+﻿namespace TappiExcercises.Domain
+{
+    public class Grade
+    {
+
+        private double _value;
+        private string _subject; 
+
+        public Grade(double value, string subject)
+        {
+            _value = value;
+            _subject = subject;
+            DateTime _date = DateTime.Now;
+
+        }
+
+        public string GetSubject() => _subject;
+        public double GetValue() => _value;
+
+
+
+
+
+    }
+}
---
+++ src/TappiExcercises.Domain/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TappiExcercises.Domain
+{
+    public class Student
+    {
+
+
+        private int Id { get; }
+        private string Name { get; }
+        private string Surname { get; }
+        private string Class {  get; }
+
+        List<Grade> _grades;
+        HashSet<String> _subjects;
+
+        public Student(int id, string name, string surname, string c)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            Class = c;
+            _grades = new List<Grade>();
+        }
+
+        public void AddGrade(double value, string subject)
+        {
+            _grades.Add(new Grade(value, subject));
+
+        }
+
+        private void GetStudentSubjects()
+        {
+            for(int i = 0; i < _grades.Count; i++)
+            {
+
+                _subjects.Add(_grades[i].GetSubject);
+            }
+        }
+    }
+}
