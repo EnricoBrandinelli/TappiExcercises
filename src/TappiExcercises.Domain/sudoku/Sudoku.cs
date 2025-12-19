ee7652b494342aaa7a1e68facca6b12f34ac7ea7
EnricoBrandinelli <enrico.brandinelli.stud@ispascalcomandini.it>
2025-12-19 09:51:39 +0100

    classwork

---
+++ src/TappiExcercises.Domain/sudoku/Sudoku.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TappiExcercises.Domain.sudoku
+{
+    public class Sudoku
+    {
+        public bool CheckSubMatrix(int minr, int maxr, int minc, int maxc, int[,] matrix)
+        {
+            List<int> check = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            for(int r = minr; r<= maxr; r++)
+            {
+                for(int c = minc; c<=maxc; c++)
+                {
+                    foreach(int i in check)
+                    {
+                        if (matrix[r,c] == i)
+                        {
+                            check.Remove(i);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (check.Count == 0)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public bool IsSudoku(int[,] matrix)
+        {
+            List<int> check = new List<int>();
+            
+            for(int r = 0; r<matrix.GetLength(0); r++)
+            {
+                for(int c = 0; c<matrix.GetLength(1); c++)
+                {
+                    foreach(int i in check)
+                    {
+                        if (matrix[r,c] == i)
+                        {
+                            check.Remove(i);
+                            break;
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
